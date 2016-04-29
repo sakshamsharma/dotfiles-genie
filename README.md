2a128d638de29c1b5d6038e8db9f0b71246dde4e
Saksham Sharma <saksham0808@gmail.com>
2016-04-29 15:58:44 +0530

    Add support for directories.

--- README.md
+++ README.md
@@ -2,9 +2,8 @@ dotfiles-genie
 ==============
 
 My configuration management system, a mix of ideas from GNU stow, vcsh, Mr and some other utilities.
-Thanks @pallavagarwal07 for the idea on symlinks.
 
 More detail coming soon. For now, place any files in this folder, and execute `./genie.py conf.yaml` to place files wherever they are meant to be.
 The config file will dictate everything about the file structure you desire. Look at it for a sample.
 
-The initial 150 LOC were written on a train trip back to home.
+The initial 250 LOC were written on a train trip back to home.
--- genie.py
+++ genie.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import shutil
+import distutils
 
 import yaml
 import filecmp
@@ -13,7 +14,11 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-def recursiveAction(src, dest, action):
+# TODO
+# Move file and folder stuff to new files
+# Abstract out the asking user for options part into a global helper function
+
+def recursiveAction(src, dest, action, fileOrDir):
     """
     Copy or symlink file from source to dest.  dest can include an absolute or relative path
     If the path doesn't exist, it gets created
@@ -27,14 +32,30 @@ def recursiveAction(src, dest, action):
     except os.error as e:
         pass #Assume it exists.  This could fail if you don't have permissions, etc...
 
+
+    # If there's a symlink there already, get it out
+    if os.path.islink(dest):
+        os.unlink(dest)
+
+    # Copy all files, no symlinking
     if action == 1:
         # Use copy2 to preserve almost all metadata like modification time etc
         # Do not follow symlinks as it can overwrite the original file too
-        os.remove(dest)
-        shutil.copy2(src, dest, follow_symlinks=False)
-    else:
-        os.symlink(os.path.join(genieCwd, _file['placed']), _location)
 
+        # Copy according to file/directory behavior
+        if fileOrDir == "dir":
+            # shutil.copytree(src, dest, ignore_errors=True, copy_function=copy2)
+            distutils.dir_util.copy_tree(src, dest)
+        else:
+            shutil.copy2(src, dest, follow_symlinks=False)
+
+    # Default symlink behaviour
+    else:
+        os.symlink(src, dest)
+
+"""
+The config is read and parsed as this class
+"""
 class Configuration:
     def __init__(self, _files=None, _directories=None, _subrepos=None):
         self.files = _files or []
@@ -48,6 +69,7 @@ def readConfig(fileName):
     fileDes = open(fileName, 'r')
     return yaml.load(fileDes)
 
+# TODO Check for sys.argv and set a default
 print("Starting genie..")
 configObject = readConfig(sys.argv[1])
 
@@ -87,6 +109,7 @@ for _file in configObject.files:
         # Can specify ignoreDiff key in config file to replace without diffing
         # Otherwise, default is to replace
         # TODO First check if the files are the same, to save diff times
+        # TODO Allow disabling backups globally in config file as well
         _modified = True
         if not "ignoreDiff" in _file:
             _modified = not filecmp.cmp(_readFrom, _file['placed'])
@@ -121,7 +144,7 @@ for _file in configObject.files:
                 if _toBackup == 1:
                     # TODO allow multiple backups to persist
                     print("  Backing up file")
-                    recursiveAction(_readFrom, genieCwd + "/.backups/" + _file['placed'], 1)
+                    recursiveAction(_readFrom, genieCwd + "/.backups/" + _file['placed'], 1, "file")
 
         else:
             print("  Files are same, not copying")
@@ -141,10 +164,92 @@ for _file in configObject.files:
             os.remove(_location)
         except:
             pass
-        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 2)
+        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 2, "file")
         print("Symlinked " + _file['placed'] + " to " + _location)
 
     # Copy if user had specified so in the config
     elif action == 3:
-        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 1)
+        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 1, "file")
+        print("Copied " + _file['placed'] + " to " + _location)
+
+print("")
+print("")
+# Handle and place all folders
+for _file in configObject.directories:
+    _location = os.path.expandvars(_file['location'])
+    # Check if this folder already exists
+    print("Placing folder: " + _file['name'])
+
+    # Action to be taken on folder
+    """
+      0 -> Place symlink of the folder there
+      1 -> Don't copy, same folder
+      2 -> User aborted copy
+      3 -> Place the folder there, no symlinks
+    """
+    action = 0
+
+    # Status of this file
+    status = 0
+
+    if os.path.isdir(_location):
+        # Status 1 for folder exists
+        status = 1
+        print("  Already exists")
+
+        _readFrom = _location
+        if os.path.islink(_location):
+            # Status 2 if file is symlinked
+            status = 2
+            _readFrom = os.path.realpath(_location)
+            print("  Already symlinked to " + _readFrom)
+
+            # 2 means not known
+            _toBackup = 2
+            if "action" in _file:
+                if _file['action'] == "backup":
+                    _toBackup = 1
+                elif _file['action'] == "nobackup":
+                    _toBackup = 0
+                elif _file['action'] == "donothing":
+                    action = 2
+
+            # If nothing was written, ask user
+            if (_toBackup == 1 or _toBackup == 2) and action != 2:
+                if _toBackup == 2:
+                    print("  Backup, Replace or skip? (b,r,s): ")
+                    inp = input()
+                    if inp == "r":
+                        _toBackup = 0
+                    elif inp == "s":
+                        _toBackup = 0
+                        action = 2
+                    else:
+                        _toBackup = 1
+
+                if _toBackup == 1:
+                    # TODO allow multiple backups to persist
+                    print("  Backing up file")
+                    recursiveAction(_readFrom, genieCwd + "/.backups/" + _file['placed'], 1, "dir")
+
+    # Skip this file now if user skipped, or files were same
+    if action == 1 or action == 2:
+        continue
+
+    # See if this file has to be plain copied or not
+    if "plainCopy" in _file:
+        action = 3
+
+    # Symlink if its plain old symlinked based action
+    if action == 0:
+        try:
+            shutil.rmtree(_location, ignore_errors=True)
+        except:
+            pass
+        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 2, "dir")
+        print("Symlinked " + _file['placed'] + " to " + _location)
+
+    # Copy if user had specified so in the config
+    elif action == 3:
+        recursiveAction(os.path.join(genieCwd, _file['placed']), _location, 1, "dir")
         print("Copied " + _file['placed'] + " to " + _location)
